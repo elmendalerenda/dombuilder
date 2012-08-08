2898017b5bded4bfa38c29ba443fe018c8834155
Miguel Angel <mike@becodemyfriend.com>
2012-08-08 15:48:05 +0200

    dom created

--- dombuilder.js
+++ dombuilder.js
@@ -1,9 +1,31 @@
 
 (function dombuilder(){
 	
+	var itemsToDisplay = [{name:0},{name:1},{name:2},{name:3},{name:4},{name:5}];
 
-	console.log('build my dom');
+
+	var list = document.createElement('ul');
+	list.className = 'mainlist';
 
 
+	for(var i = 0; i < itemsToDisplay.length; i++){
+
+
+		var item = document.createElement('li');
+		item.className = 'item';
+
+		var text = document.createElement('p');
+		text.innerHTML = 'The element name is ' + itemsToDisplay[i].name;
+
+		var input = document.createElement('input');
+		input.type = 'text';
+
+		item.appendChild(text);
+		item.appendChild(input);
+
+		list.appendChild(item);
+	}
+
+	document.getElementById('main').appendChild(list);
 
 })();
