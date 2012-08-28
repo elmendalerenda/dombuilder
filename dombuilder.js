207c05bf787373db2c5412e935467b2c196e19a9
elmendalerenda <elmendalerenda@gmail.com>
2012-08-28 12:20:21 +0200

    dombuilder rewritten with TDD

--- dombuilder.js
+++ dombuilder.js
@@ -1,30 +1,53 @@
-var DomBuilder = new function(){
+var DOMBUILDER = (function(document) {
 
-	this.elementBuilder = function(tagName) {
+	var elementBuilder = function(tagName) {
+			return function(children, properties) {
+				children = checkChildren(children);
 
-		return function(children, props) {
+				var element = document.createElement(tagName);
+				element = appendChildren(children, element);
+				element = appendProperties(properties, element);
+				return element;
+			};
+		};
+
+	var checkChildren = function(children) {
 			children = children || [];
-			props = props || [];
+			if (children instanceof Array) return children;
+			return [children];
+		};
+	var appendChildren = function(children, emptyElement) {
 
-			var element = document.createElement(tagName);
-
-			element = children.reduce(function(reducedElement, child) {
+			var element = children.reduce(function(reducedElement, child) {
 				var innerElement = child;
 				if (typeof innerElement === 'string') innerElement = document.createTextNode(innerElement);
 
 				reducedElement.appendChild(innerElement);
 				return reducedElement;
-			}, element);
+			}, emptyElement);
 
-			for (var propertyName in props) {
-				element[propertyName] = props[propertyName];
-			}
 			return element;
 		};
-	};
 
-	this.liBuilder = this.elementBuilder('li');
-	this.spanBuilder = this.elementBuilder('span');
-	this.inputBuilder = this.elementBuilder('input');
-	this.ulBuilder = this.elementBuilder('ul');
-};
+	var appendProperties = function(properties, element) {
+			properties = properties || [];
+
+			for (var propertyName in properties) {
+				element[propertyName] = properties[propertyName];
+			}
+
+			return element;
+		};
+
+	return {
+		elementBuilder: elementBuilder,
+		divBuilder : elementBuilder('div'),
+		pBuilder : elementBuilder('p'),
+		aBuilder : elementBuilder('a'),
+		imgBuilder : elementBuilder('img'),
+		ulBuilder : elementBuilder('ul'),
+		liBuilder : elementBuilder('li'),
+		inputBuilder : elementBuilder('input'),
+		spanBuilder : elementBuilder('span')
+	};
+})(document);
--- test/spec/dombuilder.Spec.js
+++ test/spec/dombuilder.Spec.js
@@ -1,3 +1,64 @@
 describe("dombuilder", function() {
 
+    it('creates an DOM Element', function(){
+        var element = DOMBUILDER.elementBuilder('div')();
+
+        expect(element instanceof Element).toBeTruthy();
+        expect(element.tagName).toEqual('DIV');
+    });
+
+    it('hosts an elements', function(){
+        var li = DOMBUILDER.elementBuilder('li')();
+        var element = DOMBUILDER.elementBuilder('ul')(li);
+
+
+        expect(element.firstChild.tagName).toEqual('LI');
+    });
+
+    it('hosts more than one elements', function(){
+        var aLi = DOMBUILDER.elementBuilder('li')();
+        var anotherLi = DOMBUILDER.elementBuilder('li')();
+        var element = DOMBUILDER.elementBuilder('ul')(
+            [aLi, anotherLi]);
+
+        expect(element.children.length).toBe(2);
+        expect(element.firstChild.tagName).toEqual('LI');
+    });
+
+    it('hosts text', function(){
+        var element = DOMBUILDER.elementBuilder('ul')(
+            'a test text');
+
+        expect(element.innerHTML).toEqual('a test text');
+
+        element = DOMBUILDER.elementBuilder('ul')(
+            ['a test text',
+            DOMBUILDER.elementBuilder('li')()]);
+
+        expect(element.innerHTML).toMatch('a test text');
+        expect(element.children[0].tagName).toEqual('LI');
+    });
+
+    it('assign properties to the element', function(){
+        var element = DOMBUILDER.elementBuilder('div')(
+            null,
+            {className: 'aClassName', id: 'uniqueId'});
+
+        expect(element.className).toEqual('aClassName');
+        expect(element.id).toEqual('uniqueId');
+    });
+
+    it('has built-in builders', function(){
+        expect(DOMBUILDER.divBuilder().tagName).toEqual('DIV');
+        expect(DOMBUILDER.pBuilder().tagName).toEqual('P');
+        expect(DOMBUILDER.aBuilder().tagName).toEqual('A');
+        expect(DOMBUILDER.imgBuilder().tagName).toEqual('IMG');
+        expect(DOMBUILDER.ulBuilder().tagName).toEqual('UL');
+        expect(DOMBUILDER.liBuilder().tagName).toEqual('LI');
+        expect(DOMBUILDER.inputBuilder().tagName).toEqual('INPUT');
+        expect(DOMBUILDER.spanBuilder().tagName).toEqual('SPAN');
+    });
+
+
+
 });
